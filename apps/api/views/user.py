318736477924dba55255a55462ec1dd31d3879b0
Sean Harrison <sah@bookgenesis.com>
2019-10-21 08:33:15 -0500

    Add middle_name to User serializer

--- apps/api/views/user.py
+++ apps/api/views/user.py
@@ -43,6 +43,8 @@ class UserSerializer(serializers.Serializer):
         max_length=255, source='user.username')
     given_name = serializers.CharField(
         max_length=255, source='user.first_name')
+    middle_name = serializers.CharField(
+        max_length=255, source='user.userprofile.middle_name', required=False)
     family_name = serializers.CharField(
         max_length=255, source='user.last_name')
     gender = serializers.ChoiceField(choices=SEX_CHOICES, source='sex')
@@ -56,8 +58,9 @@ class UserSerializer(serializers.Serializer):
     picture = serializers.ImageField(required=False)
 
     def create(self, validated_data):
-        # raise Exception(validated_data)
         user_data = validated_data.get('user', {})
+
+        # Ensure that the username doesn't already exist
         if User.objects.filter(username=user_data['username']).exists():
             raise ValidationError(
                 'Could not create user with that username. Please choose another.', code=400)
