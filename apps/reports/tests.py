df2f6ba04a0ddcbc49928d160c95332996b815a4
Alan Viars <aviars@videntity.com>
2019-11-23 20:49:39 -0500

    Add a test

--- apps/reports/tests.py
+++ apps/reports/tests.py
@@ -1,3 +1,20 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class GenerateReportTest(TestCase):
+    # Includes two orgs, ACME Health and Transparent Health
+    fixtures = ['sample-test-orgs']
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('orgs_and_agents_report')
+        self.client.login(username="poc", password="pocpassword")
+
+    def test_report_generation(self):
+        """
+        test_report_generation
+        """
+        response = self.client.get(self.url, follow=True)
+        self.assertEqual(response.status_code, 200)
